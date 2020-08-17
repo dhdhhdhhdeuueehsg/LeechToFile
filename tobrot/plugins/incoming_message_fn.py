7e962523eeba60b3719fdfa9f1ee6bb778d74cc6
๓คгςยร <65294906+prgofficial@users.noreply.github.com>
2020-08-17 22:58:33 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -153,15 +153,13 @@ async def incoming_youtube_dl_f(client, message):
 #playlist
 async def g_yt_playlist(client, message):
     """ /pytdl command """
-    #i_m_sefg = await message.reply_text("Processing...you should wait🤗", quote=True)
-    usr_id = message.from_user.id
-    G_DRIVE = False
+    i_m_sefg = await message.reply_text("Downloading all videos from given playlist. Please wait! 🥱", quote=True)
 #    if len(message.command) > 1:
 #        if message.command[1] == "gdrive":
-#            G_DRIVE = True
-    if 'youtube.com/playlist' in message.reply_to_message.text:
-        i_m_sefg = await message.reply_text("Downloading all videos from given playlist. Please wait! 🥱", quote=True)
-        await yt_playlist_downg(message.reply_to_message, i_m_sefg, G_DRIVE)
-    
+#            with open('blame_my_knowledge.txt', 'w+') as gg:
+#                gg.write("I am noob and don't know what to do that's why I have did this")
+    if 'www.youtube.com/playlist' in message.reply_to_message.text:
+        await yt_playlist_downg(message.reply_to_message, i_m_sefg)
+
     else:
-        await message.reply_text("This command works for youtube playlists only!!")
+        await i_m_sefg.edit_text("This command works for youtube playlists only!!")
